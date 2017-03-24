07114eb0566da8dfbfa5c256ebfd8df1afd2294a
Jen O'Byrne <jennifer.obyrne@xero.com>
2017-03-24 21:09:59 +1300

    Get the rest of the tests passing
    Adding a for loop to calculated the sum of numbers that have been squared.
    Adding a method which calls the other 2 methods and generates the difference between them.
    Tests now pass. Hooray.

--- DifferenceOfSquares.cs
+++ DifferenceOfSquares.cs
@@ -15,13 +15,36 @@ public static class Squares
         return (int) square;
     }
 
+    public static int SquareOfSums(int max)
+    {
+        var sum = 0;
+        for (var i = 1; i <= max; i++)
+        {
+            sum = sum + i;
+        }
+
+        var square = Math.Pow(sum, 2);
+
+        return (int)square;
+    }
+
     public static int SumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var sum = 0;
+        for (var i = 1; i <= max; i++)
+        {
+            sum = sum + (int) Math.Pow(i, 2);
+        }
+
+        return sum;
     }
 
     public static int DifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var squaredSums = SquareOfSums(max);
+        var sumsSquared = SumOfSquares(max);
+        var difference = squaredSums - sumsSquared;
+
+        return difference;
     }
 }
